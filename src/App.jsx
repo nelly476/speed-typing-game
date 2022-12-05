9c8c95c7f9bb49a8f19224d40b6fcbaed9eac3be
nelly476 <nellybulatova12@gmail.com>
2022-12-05 11:11:49 +0300

    add general layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
 
 function App() {
+  const [text, setText] = useState("");
+
+  function handleChange(e) {
+    setText(e.target.value);
+  }
 
   return (
-   
-  )
+    <div>
+      <h1>How fast do you type?</h1>
+      <textarea onChange={handleChange} value={text} />
+      <h4>Time remaining: ???</h4>
+      <button>Start</button>
+      <h1>Word count: ???</h1>
+    </div>
+  );
 }
 
-export default App
+export default App;
