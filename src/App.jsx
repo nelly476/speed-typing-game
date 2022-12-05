db1e78cf59011809ebfc6fee3b15457e4cdb9c62
nelly476 <nellybulatova12@gmail.com>
2022-12-05 11:43:17 +0300

    add useEffect for time remaining, time counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
 
 function App() {
   const [text, setText] = useState("");
+  const [timeRemaining, setTimeRemaining] = useState(5);
+
+  useEffect(() => {
+    if (timeRemaining > 0) {
+      setTimeout(() => {
+        setTimeRemaining((prevTime) => prevTime - 1);
+      }, 1000);
+    }
+  }, [timeRemaining]);
 
   function handleChange(e) {
     setText(e.target.value);
@@ -18,7 +27,7 @@ function App() {
     <div>
       <h1>How fast do you type?</h1>
       <textarea onChange={handleChange} value={text} />
-      <h4>Time remaining: ???</h4>
+      <h4>Time remaining: {timeRemaining} </h4>
       <button onClick={() => calculateWordCount(text)}>Start</button>
       <h1>Word count: ???</h1>
     </div>
