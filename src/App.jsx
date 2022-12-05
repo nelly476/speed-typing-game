bed431981b28c9d716b780ca6b10f5921160d7f3
nelly476 <nellybulatova12@gmail.com>
2022-12-05 11:24:43 +0300

    add word counter

--- src/App.jsx
+++ src/App.jsx
@@ -9,12 +9,17 @@ function App() {
     setText(e.target.value);
   }
 
+  function calculateWordCount(input) {
+    const wordsArr = input.trim().split(" ");
+    return wordsArr.filter((word) => word !== "").length;
+  }
+
   return (
     <div>
       <h1>How fast do you type?</h1>
       <textarea onChange={handleChange} value={text} />
       <h4>Time remaining: ???</h4>
-      <button>Start</button>
+      <button onClick={() => calculateWordCount(text)}>Start</button>
       <h1>Word count: ???</h1>
     </div>
   );
